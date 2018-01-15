f9fa739d5abeb6ea716a83e99fef04adec0aab5d
Yosvel Quintero <yosvelquintero@mail.com>
2018-01-15 06:31:59 +0100

    Improved html structure and global variable APP_NAME

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Constants } from '@app/app.constants';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  public appName: string;
+
+  public ngOnInit() {
+    this.appName = Constants.APP_NAME;
+  }
+
 }
--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,3 +1,4 @@
 export class Constants {
+  public static APP_NAME = 'Contacts!';
   public static CONTACTS = 'app.contacts';
 }
